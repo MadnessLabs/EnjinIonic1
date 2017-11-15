28c96497a7f774c1efdee76ee8bec3826e1f85a4
Bobby Johnson <bobby@madnesslabs.net>
2017-11-15 01:24:01 -0600

    Fix bug with html compiel

--- services/build/html.js
+++ services/build/html.js
@@ -1,9 +1,9 @@
 const gulp = require('gulp');
 
-const htmlCompile = require('../html/inject');
+const htmlCompile = require('../html/compile');
 const buildRun = require('./run');
 
-const buildHtml = function(callback) {
+const buildHtml = function (callback) {
     buildRun('html', [htmlCompile], callback);
 };
 buildHtml.displayName = 'Building HTML files...';
--- services/html/compile.js
+++ services/html/compile.js
@@ -6,12 +6,12 @@ const cache = require('gulp-cached');
 
 const htmlInject = require('./inject');
 
-const htmlCompile = function(callback) {
+const htmlCompile = function (callback) {
     var ext = global.enjin.html.srcFile.split('.').pop();
     gulp.src(global.enjin.html.watch)
         .pipe(cache('htmlCompile'))
         .pipe(plumber({
-            errorHandler: function(error) {
+            errorHandler: function (error) {
                 if (global.isWatching && global.synced) {
                     console.log(error.message);
                     this.emit('end');
@@ -21,15 +21,15 @@ const htmlCompile = function(callback) {
         .pipe(pug({
             locals: global.enjin,
             pretty: true
-        }))        
-        .pipe(rename(function(file){
-            if(file.basename+'.'+ext === global.enjin.html.srcFile){
+        }))
+        .pipe(rename(function (file) {
+            if (file.basename + '.' + ext === global.enjin.html.srcFile) {
                 file.basename = 'index';
                 file.dirname = '../';
             }
         }))
         .pipe(gulp.dest(global.enjin.root + global.enjin.html.dir))
-        .on('end', function() {
+        .on('end', function () {
             if (callback && typeof callback === 'function') {
                 callback();
             }
